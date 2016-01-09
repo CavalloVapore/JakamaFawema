7f5635058ae7681f7bdafd85bfa44bf51be9b60b
CavalloVapore <junk.gamersjunk@googlemail.com>
2016-01-09 20:26:14 +0100

    implemented moving platform

--- JakamaFawema/Assets/Scripts/Platform/PlatformSlider.cs
+++ JakamaFawema/Assets/Scripts/Platform/PlatformSlider.cs
@@ -5,10 +5,6 @@ public class PlatformSlider : MonoBehaviour
 {
     public float speed = 3f;
 
-    public float playerspeed = 3f;
-
-    private float move = 0f;
-
     // Use this for initialization
     void Start()
     {
@@ -18,22 +14,8 @@ public class PlatformSlider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        move = 0;
-
         float t = Input.GetAxis("Slide");
 
         transform.position = new Vector3(transform.position.x + t*speed*Time.deltaTime,transform.position.y);
-
-        move = t * speed * Time.deltaTime;
-    }
-
-    void OnCollisionStay2D(Collision2D other)
-    {
-        Debug.Log("Blubb");
-        if (other.gameObject.tag == "Player")
-        {
-            Debug.Log("1");
-            other.transform.position = new Vector3(transform.position.x, other.transform.position.y);
-        }
     }
 }
