66467f2e325207b9022a6e4655e07f6d4e72e010
CavalloVapore <junk.gamersjunk@googlemail.com>
2016-01-09 19:35:04 +0100

    trying to implement moving platforms that work with character

---
+++ JakamaFawema/Assets/Scripts/Platform/PlatformSlider.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformSlider : MonoBehaviour
+{
+    public float speed = 3f;
+
+    public float playerspeed = 3f;
+
+    private float move = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = 0;
+
+        float t = Input.GetAxis("Slide");
+
+        transform.position = new Vector3(transform.position.x + t*speed*Time.deltaTime,transform.position.y);
+
+        move = t * speed * Time.deltaTime;
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        Debug.Log("Blubb");
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("1");
+            other.transform.position = new Vector3(transform.position.x, other.transform.position.y);
+        }
+    }
+}
--- JakamaFawema/Assets/Scripts/Platform/PlatformTurning.cs
+++ JakamaFawema/Assets/Scripts/Platform/PlatformTurning.cs
@@ -1,15 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
-public class PlatformTurning : MonoBehaviour {
+public class PlatformTurning : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float speed = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Input.GetAxis("Vertical");
+
+        transform.RotateAround(transform.position, Vector3.forward, t * speed*Time.deltaTime);
+    }
 }
