c4f0d9c38e9314c86a9c0c0b051fbf9ab5bb9fe6
CavalloVapore <junk.gamersjunk@googlemail.com>
2016-01-10 19:40:17 +0100

    added functionality for both turners

--- JakamaFawema/Assets/Scripts/Platform/PlatformTurning.cs
+++ JakamaFawema/Assets/Scripts/Platform/PlatformTurning.cs
@@ -6,18 +6,39 @@ public class PlatformTurning : MonoBehaviour
 {
 
     public float speed = 3f;
+    public bool turner1 = true;
+    public bool turner2 = false;
 
     // Use this for initialization
     void Start()
     {
-
+        CheckTurnerAcive();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float t = Input.GetAxis("Vertical");
+        CheckTurnerAcive();
 
-        transform.RotateAround(transform.position, Vector3.forward, t * speed*Time.deltaTime);
+        if (turner1)
+        {
+            float t = Input.GetAxis("Vertical");
+
+            transform.RotateAround(transform.position, Vector3.forward, t * speed * Time.deltaTime);
+        }
+        else
+        {
+            float t = Input.GetAxis("Vertical");
+
+            transform.RotateAround(transform.position, Vector3.forward, t * speed * Time.deltaTime);
+        }
+    }
+
+    void CheckTurnerAcive()
+    {
+        if(turner1 == turner2)
+        {
+            turner2 = !turner2;
+        }
     }
 }
