092d3eca3145a7156e02a156b203b1510616b6f6
CavalloVapore <junk.gamersjunk@googlemail.com>
2016-01-10 19:55:45 +0100

    added the same for the sliders

--- JakamaFawema/Assets/Scripts/Platform/PlatformSlider.cs
+++ JakamaFawema/Assets/Scripts/Platform/PlatformSlider.cs
@@ -4,18 +4,39 @@ using System.Collections;
 public class PlatformSlider : MonoBehaviour
 {
     public float speed = 3f;
+    public bool slider1 = true;
+    public bool slider2 = false;
 
     // Use this for initialization
     void Start()
     {
-
+        CheckSliderAcive();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float t = Input.GetAxis("Slide");
+        CheckSliderAcive();
 
-        transform.position = new Vector3(transform.position.x + t*speed*Time.deltaTime,transform.position.y);
+        if (slider1)
+        {
+            float t = Input.GetAxis("Slide");
+
+            transform.position = new Vector3(transform.position.x + t * speed * Time.deltaTime, transform.position.y);
+        }
+        else
+        {
+            float t = Input.GetAxis("Slide2");
+
+            transform.position = new Vector3(transform.position.x + t * speed * Time.deltaTime, transform.position.y);
+        }
+    }
+
+    void CheckSliderAcive()
+    {
+        if (slider1 == slider2)
+        {
+            slider2 = !slider2;
+        }
     }
 }
