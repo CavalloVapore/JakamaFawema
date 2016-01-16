51b297e0fe480cf9df7ed24d8ab637c2a93ddd9f
CavalloVapore <junk.gamersjunk@googlemail.com>
2016-01-16 18:44:56 +0100

    added lamp prefab

--- JakamaFawema/Assets/Scripts/Lamp.cs
+++ JakamaFawema/Assets/Scripts/Lamp.cs
@@ -12,14 +12,51 @@ public class Lamp : MonoBehaviour
         yellow,
     }
 
-    public Color LED;
+    public Color led;
     
+    void Start()
+    {
+
+        //TODO
+        //all LEDs off
+        gameObject.transform.GetChild(0).GetComponentInChildren<SpriteRenderer>().color = new UnityEngine.Color(1, 1, 1);
+        gameObject.transform.GetChild(0).GetComponentInChildren<SpriteRenderer>().enabled = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            
+            switch (led)
+            {
+                case (Color.red):
+                    //TODO
+                    //LED red on
+                    gameObject.transform.GetChild(0).GetComponentInChildren<SpriteRenderer>().enabled = true;
+                    gameObject.transform.GetChild(0).GetComponentInChildren<SpriteRenderer>().color = new UnityEngine.Color(1, 0, 0);
+                    break;
+
+                case (Color.blue):
+                    //TODO
+                    //LED blue on
+                    gameObject.transform.GetChild(0).GetComponentInChildren<SpriteRenderer>().enabled = true;
+                    gameObject.transform.GetChild(0).GetComponentInChildren<SpriteRenderer>().color = new UnityEngine.Color(0, 0, 1);
+                    break;
+
+                case (Color.green):
+                    //TODO
+                    //LED green on
+                    gameObject.transform.GetChild(0).GetComponentInChildren<SpriteRenderer>().enabled = true;
+                    gameObject.transform.GetChild(0).GetComponentInChildren<SpriteRenderer>().color = new UnityEngine.Color(0, 1, 0);
+                    break;
+
+                case (Color.yellow):
+                    //TODO
+                    //LED yellow on
+                    gameObject.transform.GetChild(0).GetComponentInChildren<SpriteRenderer>().enabled = true;
+                    gameObject.transform.GetChild(0).GetComponentInChildren<SpriteRenderer>().color = new UnityEngine.Color(1, 1, 0);
+                    break;
+            }
         }
     }
 }
