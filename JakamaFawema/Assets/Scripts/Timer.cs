b7bcd54894a93eecf6bcda7149aca8960e5e49b0
unknown <maxxxmayer@gmail.com>
2016-01-18 22:14:32 +0100

    Added timer for time display, can be used to shut the game

---
+++ JakamaFawema/Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float timer = 0.0f;
+    public Text timerLabel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+
+        var minutes = timer / 60; //Divide the guiTime by sixty to get the minutes.  //aus dem Inet funzt nicht ganz wies soll
+        var seconds = timer % 60;//Use the euclidean division for the seconds.
+        var fraction = (timer * 100) % 100;
+
+        //update the label value
+        timerLabel.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+
+        if (timer <= 0)
+        {
+
+        }
+	}
+}
