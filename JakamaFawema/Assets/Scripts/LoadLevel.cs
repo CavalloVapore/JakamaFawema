38e1c38b633e665fad00312457bdd7e649d46ed6
CavalloVapore <junk.gamersjunk@googlemail.com>
2016-01-21 20:33:41 +0100

    implemented all screens

---
+++ JakamaFawema/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Load(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
