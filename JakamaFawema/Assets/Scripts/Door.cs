b707806dac2e9c927bbbee087e1fa8a7ada711ac
CavalloVapore <junk.gamersjunk@googlemail.com>
2016-01-21 19:47:12 +0100

    added end, start and lose screen and the door

---
+++ JakamaFawema/Assets/Scripts/Door.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    [HideInInspector]
+    public bool red;
+    [HideInInspector]
+    public bool blue;
+    [HideInInspector]
+    public bool green;
+    [HideInInspector]
+    public bool yellow;
+
+    void Start()
+    {
+        red = false;
+        blue = false;
+        green = false;
+        yellow = false;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if( red && blue && green && yellow)
+            {
+                SceneManager.LoadScene("WinScreen");
+            }
+        }
+    }
+}
--- JakamaFawema/Assets/Scripts/Timer.cs
+++ JakamaFawema/Assets/Scripts/Timer.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour {
 
@@ -24,7 +25,7 @@ public class Timer : MonoBehaviour {
 
         if (timer <= 0)
         {
-
+            SceneManager.LoadScene("LoseScreen");
         }
 	}
 }
